a9118384cfb6b859e82cf1ad2062a20e9da0b9fe
Filip Nowicki <filfil8091@wp.pl>
2023-03-11 17:18:51 +0100

    commit

---
+++ mainpage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dynamic Website</title>
+    <link rel = "stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <section class = "main">
+<nav>
+
+<a href="#" class="logo">
+    <img src="logo1.png"/>
+</a>
+<ul class-"menu">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Features</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Contact</a></li>
+</nav>  
+
+</body>
+</html>
